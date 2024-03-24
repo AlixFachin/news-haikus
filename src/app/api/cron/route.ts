e7a51f9d0225f010f6258c6bfb9c422894ff3d44
AlixFachin <78192415+AlixFachin@users.noreply.github.com>
2024-03-24 10:58:03 +0900

    Adding a 404 page (#19)
    * fix: adding logs to API route
    
    * adding a 404 page

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -4,14 +4,21 @@ import dayjs from "dayjs";
 
 const HAIKUS_GENERATED_IN_ROUTE = 3;
 
+// Forcing the route to be revalidated on every request
+// TODO: Once stabilized, can increase the revalidation cache to a bit less than one day.
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const revalidate = 0;
+
 export async function GET(request: Request) {
   // We will call the function which creates the haikus
-  // TODO: Protect the API route with a secret token
   try {
     const date = dayjs();
     console.log(`Cron job started at ${date.format("YYYY-MM-DD HH:mm:ss")}`);
     await generateHaikusIfNeeded(date.toDate(), HAIKUS_GENERATED_IN_ROUTE);
-    return NextResponse.json({ status: "Success" }, { status: 200 });
+    return NextResponse.json(
+      { status: "Success", date: date.format("YYYY-MM-DD HH:mm:ss") },
+      { status: 200 },
+    );
   } catch (e) {
     let err = e as Error;
     return NextResponse.json({ error: err.message }, { status: 500 });
