e847ad7bc5450e15c1845907e72b988dd0874524
AlixFachin <78192415+AlixFachin@users.noreply.github.com>
2024-05-05 16:11:58 +0900

    feat: Adding e2e testing (#29)
    * feat: first commit of cypress e2e tests
    
    * add GH secrets into GitHub workflow

--- src/components/DateSwitcher.tsx
+++ src/components/DateSwitcher.tsx
@@ -7,19 +7,23 @@ import { getDateFormatJapanTimeFromDayjs } from "@/utils/datetimeUtils";
 
 export const DateSwitcher = ({ currentDate }: { currentDate: Date }) => {
   const djsDate = dayjs(currentDate);
+  const nextDayHref =
+    getDateFormatJapanTimeFromDayjs(djsDate.add(1, "day")) ===
+    getDateFormatJapanTimeFromDayjs(dayjs())
+      ? "/"
+      : `/archive/${getDateFormatJapanTimeFromDayjs(djsDate.add(1, "day"))}`;
 
   return (
     <div className="my-4 flex items-baseline justify-center rounded-lg bg-orange-400 p-4 dark:bg-blue-800">
       <Link
         href={`/archive/${getDateFormatJapanTimeFromDayjs(djsDate.subtract(1, "day"))}`}
+        data-cy="date-switcher-previous"
       >
         &lt;
       </Link>
       <div className="ml-4 mr-4 text-sm">Change Haiku Date</div>
       {djsDate.isBefore(dayjs(), "day") ? (
-        <Link
-          href={`/archive/${getDateFormatJapanTimeFromDayjs(djsDate.add(1, "day"))}`}
-        >
+        <Link href={nextDayHref} data-cy="date-switcher-next">
           &gt;
         </Link>
       ) : (
