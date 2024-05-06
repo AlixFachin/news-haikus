550c77c0330f5d2d7038160d1bc050f025442e92
Alix Fachin <alix.fachin@me.com>
2024-05-07 08:23:32 +0900

    fix: removing cron API routes from Clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["((?!^/admin).*)"],
+  publicRoutes: ["((?!^/admin).*)", "/api/cron/news", "/api/cron/generate"],
 });
 
 export const config = {
