2232da24b28d7cc780df18e5f8ad3f95977ab305
Alix Fachin <alix.fachin@me.com>
2024-05-06 20:10:26 +0900

    test: Adding scaffolding for generate e2e tests

---
+++ cypress/e2e/generate.cy.ts
@@ -0,0 +1,7 @@
+/**
+ * Haiku generation tests:
+ * Testing that the news list appears properly (requires Firebase emulator?)
+ * Testing that the "delete" button appears only for admin user (how to test the Clerk features?)
+ * Testing that the "edit" button displays the dialog
+ */
+describe("Testing the generation of haikus", () => {});
